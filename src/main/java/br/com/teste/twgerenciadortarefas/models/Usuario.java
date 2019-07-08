c52f1c84a3fa62de86466fc2dacdbfbbc7a4b0bb
marlo2222 <marlo_henrique@hotmail.com.br>
2019-07-08 20:32:42 -0300

    Definiçoes de cadastro e login

--- src/main/java/br/com/teste/twgerenciadortarefas/models/Usuario.java
+++ src/main/java/br/com/teste/twgerenciadortarefas/models/Usuario.java
@@ -6,6 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Table(name = "usr_usuario")
@@ -16,9 +19,12 @@ public class Usuario {
 	private long id;
 	
 	@Column(name = "usr_email", nullable = false, length = 100)
+	@NotNull(message = "O e-mail é obrigatorio.")
+	@Length(min = 5, max = 100, message = "O e-mail deve conter entre 5 e 100 caracteres")
 	private String email;
 	
 	@Column(name = "usr_senha", nullable = false, length = 100)
+	@NotNull(message = "A senha é obrigatoria")
 	private String senha;
 	
 	public long getId() {
